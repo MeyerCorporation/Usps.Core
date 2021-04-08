2ee038d29c8a2afc805f0ceed4d83a2e680a0a2d
Daniel R. Przybylski <danp43@outlook.com>
2021-04-08 08:56:55 -0700

    Complete zip code look up refactor

--- Core/Addresses.cs
+++ Core/Addresses.cs
@@ -8,151 +8,184 @@ using System.Xml.Linq;
 
 namespace MeyerCorp.UspsCore.Core
 {
-    public class Addresses : Api
-    {
-        /// <summary>
-        /// The Address/Standardization “Verify” API, which corrects errors in street addresses, including abbreviations and missing information, and supplies ZIP Codes and ZIP Codes + 4. The Verify API supports up to five lookups per transaction. By eliminating address errors, you will improve overall package delivery service.
-        /// </summary>
-        /// <returns></returns>
-        /// <remarks>Sample Request <code>
-        /// &lt;AddressValidateRequest USERID="XXXXXXXXXXXX"&gt;
-        /// &lt;Revision&gt;1&lt;/Revision&gt;
-        /// &lt;Address ID = "0" &gt;
-        /// &lt;Address1&gt;SUITE K&lt;/Address1&gt;
-        /// &lt;Address2&gt;29851 Aventura&lt;/Address2&gt;
-        /// &lt;City/&gt;
-        /// &lt;State&gt;CA&lt;/State&gt;
-        /// &lt;Zip5&gt;92688&lt;/Zip5&gt;
-        /// &lt;Zip4/&gt;
-        /// &lt;/Address&gt;
-        /// &lt;/AddressValidateRequest&gt;
-        /// </code></remarks>
-        public async Task<IEnumerable<Models.Address>> ValidateAsync(int revision, params Xml.Address[] addresses)
-        {
-            var xmlrequest = new AddressValidateRequest
-            {
-                UserId = UserId,
-                Revision = revision,
-                Addresses = addresses,
-            };
+	public class Addresses : Api
+	{
+		#region Validate
 
-            var id = 0;
-            foreach (var address in addresses)
-                address.Id = id++;
+		/// <summary>
+		/// The Address/Standardization “Verify” API, which corrects errors in street addresses, including abbreviations and missing information, and supplies ZIP Codes and ZIP Codes + 4. The Verify API supports up to five lookups per transaction. By eliminating address errors, you will improve overall package delivery service.
+		/// </summary>
+		/// <returns></returns>
+		/// <remarks>Sample Request <code>
+		/// &lt;AddressValidateRequest USERID="XXXXXXXXXXXX"&gt;
+		/// &lt;Revision&gt;1&lt;/Revision&gt;
+		/// &lt;Address ID = "0" &gt;
+		/// &lt;Address1&gt;SUITE K&lt;/Address1&gt;
+		/// &lt;Address2&gt;29851 Aventura&lt;/Address2&gt;
+		/// &lt;City/&gt;
+		/// &lt;State&gt;CA&lt;/State&gt;
+		/// &lt;Zip5&gt;92688&lt;/Zip5&gt;
+		/// &lt;Zip4/&gt;
+		/// &lt;/Address&gt;
+		/// &lt;/AddressValidateRequest&gt;
+		/// </code></remarks>
+		public async Task<IEnumerable<Models.Address>> ValidateAsync(int revision, params Xml.Address[] addresses)
+		{
+			var xmlrequest = new AddressValidateRequest
+			{
+				UserId = UserId,
+				Revision = revision,
+				Addresses = addresses,
+			};
 
-            Request.RequestUri = GetUrl(apiName: "Verify", type: "AddressValidateRequest", xmlrequest.ToString());
+			var id = 0;
+			foreach (var address in addresses)
+				address.Id = id++;
 
-            var response = await GetResponseStringAsync();
+			Request.RequestUri = GetUrl(apiName: "Verify", type: "AddressValidateRequest", xmlrequest.ToString());
 
-            var document = XDocument.Parse(response);
+			var response = await GetResponseStringAsync();
 
-            return document
-                .Root
-                .Elements("Address")
-                .Select(e => Models.Address.Parse(e));
-        }
+			var document = XDocument.Parse(response);
 
-        /// <summary>
-        /// The ZipCodeLookup API, which returns the ZIP Code and ZIP Code + 4 corresponding to the given address, city, and state (use USPS state abbreviations). The ZipCodeLookup API processes up to five lookups per request.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<IEnumerable<Models.ZipCode>> LookupZipCodeAsync(params Xml.Address[] addresses)       
-          {
-            var xmlrequest = new ZipCodeLookupRequest
-            {
-                Addresses = addresses,
-            };
+			return document
+				.Root
+				.Elements("Address")
+				.Select(e => Models.Address.Parse(e));
+		}
 
-            Request.RequestUri = GetUrl("ZipCodeLookup", "ZipCodeLookupRequest", xmlrequest.ToString());
+		private class AddressValidateRequest
+		{
+			public string UserId { get; set; }
+			public int Revision { get; set; }
+			public Xml.Address[] Addresses { get; set; }
 
-            var response = await GetResponseStringAsync();
+			public override string ToString()
+			{
+				var request = new StringBuilder();
 
-            var document = XDocument.Parse(response);
+				request.AppendXml("Revision", Revision.ToString());
 
-            return document
-                .Root
-                .Elements("Address")
-                .Select(e => Models.ZipCode.Parse(e));
-        }
+				foreach (var address in Addresses.Where(i => i != null))
+					request.Append(address.ToString());
 
-        /// <summary>
-        /// City/State Lookup API returns the city and state corresponding to the given ZIP Code. The CityStateLookup API processes up to five lookups per request.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<IEnumerable<Models.CityState>> LookupCityStateAsync(string zip51,
-            string zip52 = null,
-            string zip53 = null,
-            string zip54 = null,
-            string zip55 = null)
-        {
-            var xmlrequest = new LookupCityStateRequest
-            {
-                Zip51 = zip51,
-                Zip52 = zip52,
-                Zip53 = zip53,
-                Zip54 = zip54,
-                Zip55 = zip55,
-            };
+				return request.ToString();
+			}
+		}
 
-            Request.RequestUri = GetUrl("CityStateLookup", "CityStateLookupRequest", xmlrequest.ToString());
+		#endregion
 
-            var response = await GetResponseStringAsync();
+		#region ZipCode
 
-            var document = XDocument.Parse(response);
+		/// <summary>
+		/// The ZipCodeLookup API, which returns the ZIP Code and ZIP Code + 4 corresponding to the given address, city, and state (use USPS state abbreviations). The ZipCodeLookup API processes up to five lookups per request.
+		/// </summary>
+		/// <returns></returns>
+		public async Task<IEnumerable<Models.ZipCode>> LookupZipCodeAsync(params Xml.ZipCode[] addresses)
+		{
+			var xmlrequest = new ZipCodeLookupRequest
+			{
+				Addresses = addresses,
+			};
 
-            return document
-                .Root
-                .Elements("ZipCode")
-                .Select(e => Models.CityState.Parse(e));
-        }
+			Request.RequestUri = GetUrl("ZipCodeLookup", "ZipCodeLookupRequest", xmlrequest.ToString());
 
-        private class AddressValidateRequest
-        {
-            public string UserId { get; set; }
-            public int Revision { get; set; }
-            public Xml.Address[] Addresses { get; set; }
+			var response = await GetResponseStringAsync();
 
-            public override string ToString()
-            {
-                var request = new StringBuilder();
+			var document = XDocument.Parse(response);
 
-                request.AppendXml("Revision", Revision.ToString());
+			return document
+				.Root
+				.Elements("Address")
+				.Select(e => Models.ZipCode.Parse(e));
+		}
 
-                foreach (var address in Addresses.Where(i => i != null).Select(a => a.ToString()))
-                    request.Append(address);
+		private class ZipCodeLookupRequest
+		{
+			public Xml.ZipCode[] Addresses { get; set; }
 
-                return request.ToString();
-            }
-        }
+			public override string ToString()
+			{
+				var request = new StringBuilder();
 
-        private class LookupCityStateRequest
-        {
-            public string Zip51 { get; set; }
-            public string Zip52 { get; set; }
-            public string Zip53 { get; set; }
-            public string Zip54 { get; set; }
-            public string Zip55 { get; set; }
+				foreach (var address in Addresses.Where(i => i != null))
+					request.Append(address.ToString());
 
-            public override string ToString()
-            {
-                var index=0;
-                var xml= new Xml.CityState[]
-                {
-                    String.IsNullOrWhiteSpace(Zip51) ? null : new Xml.CityState { Zip5 = Zip51, Id = index++, },
-            		String.IsNullOrWhiteSpace(Zip52) ? null : new Xml.CityState { Zip5 = Zip52, Id = index++, },
-            		String.IsNullOrWhiteSpace(Zip53) ? null : new Xml.CityState { Zip5 = Zip53, Id = index++, },
-            		String.IsNullOrWhiteSpace(Zip54) ? null : new Xml.CityState { Zip5 = Zip54, Id = index++, },
-            		String.IsNullOrWhiteSpace(Zip55) ? null : new Xml.CityState { Zip5 = Zip55, Id = index++, }
-                }
-                .Where(i=>i!=null);
+				return request.ToString();
+			}
+		}
 
-                return String.Join(String.Empty,xml);
-            }
-        }
+		#endregion
 
-        private class ZipCodeLookupRequest
-        {
-            public Xml.Address[] Addresses { get;  set; }
-        }
-    }
+		#region CityState
+
+		/// <summary>
+		/// City/State Lookup API returns the city and state corresponding to the given ZIP Code. The CityStateLookup API processes up to five lookups per request.
+		/// </summary>
+		/// <returns></returns>
+		public async Task<IEnumerable<Models.CityState>> LookupCityStateAsync(string zip51,
+			string zip52 = null,
+			string zip53 = null,
+			string zip54 = null,
+			string zip55 = null)
+		{
+			var xmlrequest = new LookupCityStateRequest
+			{
+				Zip51 = zip51,
+				Zip52 = zip52,
+				Zip53 = zip53,
+				Zip54 = zip54,
+				Zip55 = zip55,
+			};
+
+			Request.RequestUri = GetUrl("CityStateLookup", "CityStateLookupRequest", xmlrequest.ToString());
+
+			var response = await GetResponseStringAsync();
+
+			var document = XDocument.Parse(response);
+
+			return document
+				.Root
+				.Elements("ZipCode")
+				.Select(e => Models.CityState.Parse(e));
+		}
+
+		private class LookupCityStateRequest
+		{
+			public string Zip51 { get; set; }
+			public string Zip52 { get; set; }
+			public string Zip53 { get; set; }
+			public string Zip54 { get; set; }
+			public string Zip55 { get; set; }
+
+			public override string ToString()
+			{
+				var index = 0;
+				var xml = new Xml.CityState[]
+				{
+					GetCityState(Zip51, index++),
+					GetCityState(Zip52, index++),
+					GetCityState(Zip53, index++),
+					GetCityState(Zip54, index++),
+					GetCityState(Zip55, index++),
+				}
+				.Where(i => i != null);
+
+				return String.Join(String.Empty, xml);
+			}
+
+			static Xml.CityState GetCityState(string zip, int index)
+			{
+				return String.IsNullOrWhiteSpace(zip)
+					? null
+					: new Xml.CityState
+					{
+						Zip5 = zip,
+						Id = index,
+					};
+			}
+		}
+
+		#endregion
+	}
 }
--- Test/Core/AddressesTest.cs
+++ Test/Core/AddressesTest.cs
@@ -7,266 +7,266 @@ using Xunit;
 
 namespace MeyerCorp.UspsCore.Test.Core
 {
-    public class AddressesTest
-    {
-        IConfiguration Configuration { get; set; }
+	public class AddressesTest
+	{
+		IConfiguration Configuration { get; set; }
 
-        const string BaseUrl = "https://secure.shippingapis.com/ShippingAPI.dll";
+		const string BaseUrl = "https://secure.shippingapis.com/ShippingAPI.dll";
 
-        public AddressesTest()
-        {
-            var builder = new ConfigurationBuilder()
-                .AddUserSecrets<AddressesTest>();
+		public AddressesTest()
+		{
+			var builder = new ConfigurationBuilder()
+				.AddUserSecrets<AddressesTest>();
 
-            Configuration = builder.Build();
-        }
+			Configuration = builder.Build();
+		}
 
-        [Fact(DisplayName = "Invalid Zip Code")]
-        public async Task Test1Async()
-        {
-            var UserId = Configuration["ApiUsername"];
+		[Fact(DisplayName = "Invalid Zip Code")]
+		public async Task Test1Async()
+		{
+			var UserId = Configuration["ApiUsername"];
 
-            var addresses = new Addresses
-            {
-                UserId = UserId,
-                BaseUrl = BaseUrl,
-            };
+			var addresses = new Addresses
+			{
+				UserId = UserId,
+				BaseUrl = BaseUrl,
+			};
 
-            var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
-                 {
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "address1",
-                    Address2 = "address2",
-                    City = "city",
-                    FirmName = "firmname",
-                    Id = 0,
-                    State = "ST",
-                    Urbanization = "urbanization",
-                    Zip4 = "9999",
-                    Zip5 = "00000",
-                }
-                 }));
+			var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
+			{
+				new UspsCore.Core.Xml.Address
+				{
+					Address1 = "address1",
+					Address2 = "address2",
+					City = "city",
+					FirmName = "firmname",
+					Id = 0,
+					State = "ST",
+					Urbanization = "urbanization",
+					Zip4 = "9999",
+					Zip5 = "00000",
+				}
+			}));
 
-            Assert.Equal("Invalid Zip Code.", ex.Message);
-        }
+			Assert.Equal("Invalid Zip Code.", ex.Message);
+		}
 
-        [Fact(DisplayName = "Address Not Found")]
-        public async Task Test2Async()
-        {
-            var UserId = Configuration["ApiUsername"];
+		[Fact(DisplayName = "Address Not Found")]
+		public async Task Test2Async()
+		{
+			var UserId = Configuration["ApiUsername"];
 
-            var addresses = new Addresses
-            {
-                UserId = UserId,
-                BaseUrl = BaseUrl,
-            };
+			var addresses = new Addresses
+			{
+				UserId = UserId,
+				BaseUrl = BaseUrl,
+			};
 
-            var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
-                 {
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "address1",
-                    Address2 = "address2",
-                    City = "city",
-                    FirmName = "firmname",
-                    Id = 0,
-                    State = "ST",
-                    Urbanization = "urbanization",
+			var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
+				 {
+				new UspsCore.Core.Xml.Address
+				{
+					Address1 = "address1",
+					Address2 = "address2",
+					City = "city",
+					FirmName = "firmname",
+					Id = 0,
+					State = "ST",
+					Urbanization = "urbanization",
 					//Zip4 = "9999",
 					Zip5 = "95687",
-                }
-                 }));
+				}
+				 }));
 
-            Assert.Equal("Address Not Found.", ex.Message);
-        }
+			Assert.Equal("Address Not Found.", ex.Message);
+		}
 
-        [Fact(DisplayName = "Single Address")]
-        public async Task Test3Async()
-        {
-            var UserId = Configuration["ApiUsername"];
+		[Fact(DisplayName = "Single Address")]
+		public async Task Test3Async()
+		{
+			var UserId = Configuration["ApiUsername"];
 
-            var addresses = new Addresses
-            {
-                UserId = UserId,
-                BaseUrl = BaseUrl,
-            };
+			var addresses = new Addresses
+			{
+				UserId = UserId,
+				BaseUrl = BaseUrl,
+			};
 
-            var results = await addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
-            {
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "2001 Meyer Way",
+			var results = await addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
+			{
+				new UspsCore.Core.Xml.Address
+				{
+					Address1 = "2001 Meyer Way",
 					//Address2 = "address2",
 					City = "Fairfield",
 					//FirmName = "firmname",
 					Id = 0,
-                    State = "CA",
+					State = "CA",
 					//Urbanization = "urbanization",
 					//Zip4 = "9999",
 					//Zip5 = "95687",
 				}
-            });
+			});
 
-            var result = results.First();
+			var result = results.First();
 
-            Assert.Equal("0", result.Id);
-            Assert.Null(result.Address1);
-            Assert.Equal("2001 MEYER WAY", result.Address2);
-            Assert.True(result.Business);
-            Assert.Equal("C037", result.CarrierRoute);
-            Assert.False(result.CentralDeliveryPoint);
-            Assert.Equal("FAIRFIELD", result.City);
-            Assert.Null(result.CityAbbreviation);
-            Assert.Equal("01", result.DeliveryPoint);
-            Assert.False(result.DPVCMRA);
-            Assert.True(result.DPVConfirmation);
-            Assert.Equal("AABB", result.DPVFootnotes);
-            Assert.Null(result.Error);
-            Assert.Null(result.FirmName);
-            Assert.Null(result.Footnotes);
-            Assert.Equal("CA", result.State);
-            Assert.Null(result.Urbanization);
-            Assert.False(result.Vacant);
-            Assert.Equal("6802", result.Zip4);
-            Assert.Equal("94533", result.Zip5);
-        }
+			Assert.Equal("0", result.Id);
+			Assert.Null(result.Address1);
+			Assert.Equal("2001 MEYER WAY", result.Address2);
+			Assert.True(result.Business);
+			Assert.Equal("C037", result.CarrierRoute);
+			Assert.False(result.CentralDeliveryPoint);
+			Assert.Equal("FAIRFIELD", result.City);
+			Assert.Null(result.CityAbbreviation);
+			Assert.Equal("01", result.DeliveryPoint);
+			Assert.False(result.DPVCMRA);
+			Assert.True(result.DPVConfirmation);
+			Assert.Equal("AABB", result.DPVFootnotes);
+			Assert.Null(result.Error);
+			Assert.Null(result.FirmName);
+			Assert.Null(result.Footnotes);
+			Assert.Equal("CA", result.State);
+			Assert.Null(result.Urbanization);
+			Assert.False(result.Vacant);
+			Assert.Equal("6802", result.Zip4);
+			Assert.Equal("94533", result.Zip5);
+		}
 
-        [Fact(DisplayName = "Double Address")]
-        public async Task Test4Async()
-        {
-            var UserId = Configuration["ApiUsername"];
+		[Fact(DisplayName = "Double Address")]
+		public async Task Test4Async()
+		{
+			var UserId = Configuration["ApiUsername"];
 
-            var addresses = new Addresses
-            {
-                UserId = UserId,
-                BaseUrl = BaseUrl,
-            };
+			var addresses = new Addresses
+			{
+				UserId = UserId,
+				BaseUrl = BaseUrl,
+			};
 
-            var results = await addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
-            {
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "2001 Meyer Way",
+			var results = await addresses.ValidateAsync(1, new UspsCore.Core.Xml.Address[]
+			{
+				new UspsCore.Core.Xml.Address
+				{
+					Address1 = "2001 Meyer Way",
 					//Address2 = "address2",
 					City = "Fairfield",
 					//FirmName = "firmname",
 					Id = 0,
-                    State = "CA",
+					State = "CA",
 					//Urbanization = "urbanization",
 					//Zip4 = "9999",
 					//Zip5 = "95687",
 				},
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "1 Meyer Plaza",
+				new UspsCore.Core.Xml.Address
+				{
+					Address1 = "1 Meyer Plaza",
 					//Address2 = "address2",
 					City = "Vallejo",
 					//FirmName = "firmname",
 					Id = 0,
-                    State = "CA",
+					State = "CA",
 					//Urbanization = "urbanization",
 					//Zip4 = "9999",
 					//Zip5 = "95687",
 				}
 
-            });
+			});
 
-            var result = results.First();
+			var result = results.First();
 
-            Assert.Equal("0", result.Id);
-            Assert.Null(result.Address1);
-            Assert.Equal("2001 MEYER WAY", result.Address2);
-            Assert.True(result.Business);
-            Assert.Equal("C037", result.CarrierRoute);
-            Assert.False(result.CentralDeliveryPoint);
-            Assert.Equal("FAIRFIELD", result.City);
-            Assert.Null(result.CityAbbreviation);
-            Assert.Equal("01", result.DeliveryPoint);
-            Assert.False(result.DPVCMRA);
-            Assert.True(result.DPVConfirmation);
-            Assert.Equal("AABB", result.DPVFootnotes);
-            Assert.Null(result.Error);
-            Assert.Null(result.FirmName);
-            Assert.Null(result.Footnotes);
-            Assert.Equal("CA", result.State);
-            Assert.Null(result.Urbanization);
-            Assert.False(result.Vacant);
-            Assert.Equal("6802", result.Zip4);
-            Assert.Equal("94533", result.Zip5);
-        }
+			Assert.Equal("0", result.Id);
+			Assert.Null(result.Address1);
+			Assert.Equal("2001 MEYER WAY", result.Address2);
+			Assert.True(result.Business);
+			Assert.Equal("C037", result.CarrierRoute);
+			Assert.False(result.CentralDeliveryPoint);
+			Assert.Equal("FAIRFIELD", result.City);
+			Assert.Null(result.CityAbbreviation);
+			Assert.Equal("01", result.DeliveryPoint);
+			Assert.False(result.DPVCMRA);
+			Assert.True(result.DPVConfirmation);
+			Assert.Equal("AABB", result.DPVFootnotes);
+			Assert.Null(result.Error);
+			Assert.Null(result.FirmName);
+			Assert.Null(result.Footnotes);
+			Assert.Equal("CA", result.State);
+			Assert.Null(result.Urbanization);
+			Assert.False(result.Vacant);
+			Assert.Equal("6802", result.Zip4);
+			Assert.Equal("94533", result.Zip5);
+		}
 
-        [Fact(DisplayName = "Double City State Lookup")]
-        public async Task Test5Async()
-        {
-            var UserId = Configuration["ApiUsername"];
+		[Fact(DisplayName = "Double City State Lookup")]
+		public async Task Test5Async()
+		{
+			var UserId = Configuration["ApiUsername"];
 
-            var addresses = new Addresses
-            {
-                UserId = UserId,
-                BaseUrl = BaseUrl,
-            };
+			var addresses = new Addresses
+			{
+				UserId = UserId,
+				BaseUrl = BaseUrl,
+			};
 
-            var results = await addresses.LookupCityStateAsync("95687", "95127");
+			var results = await addresses.LookupCityStateAsync("95687", "95127");
 
-            Assert.Equal(2, results.Count());
-            Assert.Equal("VACAVILLE", results.First().City);
-            Assert.Equal("CA", results.First().State);
-            Assert.Equal("SAN JOSE", results.Last().City);
-            Assert.Equal("CA", results.Last().State);
-        }
-    
-    
-	    [Fact(DisplayName = "Double Zip Code Lookup")]
-        public async Task Test6Async()
-        {
-            var UserId = Configuration["ApiUsername"];
+			Assert.Equal(2, results.Count());
+			Assert.Equal("VACAVILLE", results.First().City);
+			Assert.Equal("CA", results.First().State);
+			Assert.Equal("SAN JOSE", results.Last().City);
+			Assert.Equal("CA", results.Last().State);
+		}
 
-            var addresses = new Addresses
-            {
-                UserId = UserId,
-                BaseUrl = BaseUrl,
-            };
+		[Fact(DisplayName = "Double Zip Code Lookup")]
+		public async Task Test6Async()
+		{
+			var UserId = Configuration["ApiUsername"];
 
-            var results = await addresses.LookupZipCodeAsync(new UspsCore.Core.Xml.Address[]
-            {
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "2001 Meyer Way",
+			var addresses = new Addresses
+			{
+				UserId = UserId,
+				BaseUrl = BaseUrl,
+			};
+
+			var results = await addresses.LookupZipCodeAsync(new UspsCore.Core.Xml.ZipCode[]
+			{
+				new UspsCore.Core.Xml.ZipCode
+				{
+					Address1 = "2001 Meyer Way",
 					//Address2 = "address2",
 					City = "Fairfield",
 					//FirmName = "firmname",
 					Id = 0,
-                    State = "CA",
+					State = "CA",
 					//Urbanization = "urbanization",
 					//Zip4 = "9999",
 					//Zip5 = "95687",
 				},
-                new UspsCore.Core.Xml.Address
-                {
-                    Address1 = "1 Meyer Plaza",
+				new UspsCore.Core.Xml.ZipCode
+				{
+					Address1 = "1 Meyer Plaza",
 					//Address2 = "address2",
 					City = "Vallejo",
 					//FirmName = "firmname",
 					Id = 0,
-                    State = "CA",
+					State = "CA",
 					//Urbanization = "urbanization",
 					//Zip4 = "9999",
 					//Zip5 = "95687",
 				}
 
-            });
+			});
 
-            var result = results.First();
+			var result = results.First();
 
-            Assert.Equal("0", result.Id);
-            Assert.Null(result.Address1);
-            Assert.Equal("2001 MEYER WAY", result.Address2);
-            Assert.Equal("FAIRFIELD", result.City);
-            Assert.Null(result.Error);
-            Assert.Null(result.FirmName);
-            Assert.Equal("CA", result.State);
-            Assert.Equal("6802", result.Zip4);
-            Assert.Equal("94533", result.Zip5);        }
-    }
+			Assert.Equal("0", result.Id);
+			Assert.Null(result.Address1);
+			Assert.Equal("2001 MEYER WAY", result.Address2);
+			Assert.Equal("FAIRFIELD", result.City);
+			Assert.Null(result.Error);
+			Assert.Null(result.FirmName);
+			Assert.Equal("CA", result.State);
+			Assert.Equal("6802", result.Zip4);
+			Assert.Equal("94533", result.Zip5);
+		}
+	}
 }
