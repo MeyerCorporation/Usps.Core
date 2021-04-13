e0ea45a8971557b1b97ef9966e7da4eb1f7bfdf1
Daniel Przybylski <danp43@outlook.com>
2021-04-13 07:51:26 -0700

    Add XML comments

--- Core/Extentions/Xml.cs
+++ Core/Extentions/Xml.cs
@@ -4,13 +4,28 @@ using System.Text;
 
 namespace MeyerCorp.Usps.Core.Extensions
 {
+    ///<summary>
+    ///Extension methods for Xml classes in the project.
+    ///</summary>
     public static partial class Methods
     {
+        ///<summary>
+        ///Appends a string containing an XML element with optional simple content and any number of attributes.
+        ///</summary>
+        ///<param cref="builder"></param>
+        ///<param cref="tag"></param>
+        ///<param cref="value"></param>
+        ///<param cref="attributeValuePairs"></param>
+        ///<exception cref="ArgumentNullException">The string builder object which this method is applied to is null.</exception>
+        ///<exception cref="ArgumentException">The tag parameter is null, empty or whitespace.</exception>
+        ///<exception cref="ArgumentException">After filtering out any attribute tags and values in the attributeValuePairs which 
+        //are null, emtpy or whitespace, the number of remaining parameter values are counted and determined to be an odd number.</exception>
+        ///</exception>
         public static StringBuilder AppendXml(this StringBuilder builder, string tag, string value, params string[] attributeValuePairs)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
 
-            var attributescheck = attributeValuePairs.Where(avp=>!String.IsNullOrWhiteSpace(avp));
+            var attributescheck = attributeValuePairs.Where(avp => !String.IsNullOrWhiteSpace(avp));
 
             if (attributeValuePairs.Length > 0 && (attributescheck.Count() % 2) != 0)
                 throw new ArgumentException("There must be an even number of non-null, non-empty and non-whitespace attribute-value pairs as each pair represents an attribute and its value.",
