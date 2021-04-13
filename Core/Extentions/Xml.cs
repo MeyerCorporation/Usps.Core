674145cbfde7e9673dd378640a2848c58b303b26
Daniel Przybylski <danp43@outlook.com>
2021-04-12 21:19:52 -0700

    More AppendXml testing

--- Core/Extentions/Xml.cs
+++ Core/Extentions/Xml.cs
@@ -1,29 +1,37 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace MeyerCorp.Usps.Core.Extensions
 {
-	public static partial class Methods
-	{
-		public static StringBuilder AppendXml(this StringBuilder builder, string tag, string value, params string[] attributeValuePairs)
-		{
-			if(builder==null) throw new ArgumentNullException(nameof(builder));
-			
-			if (string.IsNullOrWhiteSpace(tag))
-				throw new ArgumentException();
-			else if (attributeValuePairs.Length == 0)
-				return builder.AppendFormat("<{0}>{1}</{0}>", tag, value);
-			else
-			{
-				var attributeList = new StringBuilder();
+    public static partial class Methods
+    {
+        public static StringBuilder AppendXml(this StringBuilder builder, string tag, string value, params string[] attributeValuePairs)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
 
-				for (var index = 0; index < attributeValuePairs.Length; index += 2)
-				{
-					attributeList.AppendFormat("{0}=\"{1}\" ", attributeValuePairs[index], attributeValuePairs[index + 1]);
-				}
+            var attributescheck = attributeValuePairs.Where(avp=>!String.IsNullOrWhiteSpace(avp));
 
-				return builder.AppendFormat("<{0} {2}>{1}</{0}>", tag, value, attributeList.ToString().TrimEnd());
-			}
-		}
-	}
+            if (attributeValuePairs.Length > 0 && (attributescheck.Count() % 2) != 0)
+                throw new ArgumentException("There must be an even number of non-null, non-empty and non-whitespace attribute-value pairs as each pair represents an attribute and its value.",
+                 nameof(attributeValuePairs));
+
+            if (string.IsNullOrWhiteSpace(tag))
+                throw new ArgumentException("The argument cannot be null, empty or contain only whitespace.", nameof(tag));
+
+            if (attributeValuePairs.Length == 0)
+                return builder.AppendFormat("<{0}>{1}</{0}>", tag, value);
+            else
+            {
+                var attributeList = new StringBuilder();
+
+                for (var index = 0; index < attributeValuePairs.Length; index += 2)
+                {
+                    attributeList.AppendFormat("{0}=\"{1}\" ", attributeValuePairs[index], attributeValuePairs[index + 1]);
+                }
+
+                return builder.AppendFormat("<{0} {2}>{1}</{0}>", tag, value, attributeList.ToString().TrimEnd());
+            }
+        }
+    }
 }
--- Test/Core/Extensions/ExtensionsTest.cs
+++ Test/Core/Extensions/ExtensionsTest.cs
@@ -9,7 +9,7 @@ namespace Meyer.UspsCore.Test.Core
 {
     public class ExtensionsTest
     {
-        [Fact(DisplayName = "AppendXml")]
+        [Fact(DisplayName = "AppendXml null string builder")]
         public void Test1()
         {
             var stringbuilder = default(StringBuilder);
@@ -20,6 +20,109 @@ namespace Meyer.UspsCore.Test.Core
             });
 
             Assert.Equal("Value cannot be null. (Parameter 'builder')", result.Message);
+        }
+
+
+        [Fact(DisplayName = "AppendXml null tag")]
+        public void Test2()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = Assert.Throws<ArgumentException>(() =>
+            {
+                stringbuilder.AppendXml(null, "value");
+            });
+
+            Assert.Equal("The argument cannot be null, empty or contain only whitespace. (Parameter 'tag')"
+, result.Message);
+        }
+
+
+        [Fact(DisplayName = "AppendXml empty tag")]
+        public void Test3()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = Assert.Throws<ArgumentException>(() =>
+            {
+                stringbuilder.AppendXml(String.Empty, "value");
+            });
+
+            Assert.Equal("The argument cannot be null, empty or contain only whitespace. (Parameter 'tag')"
+, result.Message);
+        }
+
+
+        [Fact(DisplayName = "AppendXml whitespace tag")]
+        public void Test4()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = Assert.Throws<ArgumentException>(() =>
+            {
+                stringbuilder.AppendXml(" " + Environment.NewLine, "value");
+            });
+
+            Assert.Equal("The argument cannot be null, empty or contain only whitespace. (Parameter 'tag')"
+, result.Message);
+        }
+
+        [Fact(DisplayName = "AppendXml tag and value")]
+        public void Test5()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = stringbuilder.AppendXml("tag", "value");
+
+            Assert.Equal("<tag>value</tag>", result.ToString());
+        }
+
+        [Fact(DisplayName = "AppendXml tag and value and single attribute")]
+        public void Test6()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = stringbuilder.AppendXml("tag", "value", "attribute", "attributevalue");
+
+            Assert.Equal("<tag attribute=\"attributevalue\">value</tag>", result.ToString());
+        }
+
+        [Fact(DisplayName = "AppendXml tag and value and double attribute")]
+        public void Test7()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = stringbuilder.AppendXml("tag", "value", "attribute", "attributevalue", "attribute1", "attributevalue1");
+
+            Assert.Equal("<tag attribute=\"attributevalue\" attribute1=\"attributevalue1\">value</tag>", result.ToString());
+        }
+
+        [Fact(DisplayName = "AppendXml tag and value and uneven attribute/value pair arguments")]
+        public void Test8()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = Assert.Throws<ArgumentException>(() =>
+            {
+                var result = stringbuilder.AppendXml("tag", "value", "attribute", "attributevalue", "attribute1");
+            });
+
+            Assert.Equal("There must be an even number of non-null, non-empty and non-whitespace attribute-value pairs as each pair represents an attribute and its value. (Parameter 'attributeValuePairs')",
+                result.Message);
+        }
+
+        [Fact(DisplayName = "AppendXml tag and value and null attribute/value pair arguments")]
+        public void Test9()
+        {
+            var stringbuilder = new StringBuilder();
+
+            var result = Assert.Throws<ArgumentException>(() =>
+            {
+                var result = stringbuilder.AppendXml("tag", "value", "attribute", null, "attribute1", "value1");
+            });
+
+            Assert.Equal("There must be an even number of non-null, non-empty and non-whitespace attribute-value pairs as each pair represents an attribute and its value. (Parameter 'attributeValuePairs')",
+                result.Message);
         }
     }
 }
