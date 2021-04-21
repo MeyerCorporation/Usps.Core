8b2fba33435a4b99fa5a3e89fc0d654d5dee74e1
Daniel R. Przybylski <danp43@outlook.com>
2021-04-21 13:27:55 -0700

    Match track and confirm interface to class

--- Core/ITrackAndConfirm.cs
+++ Core/ITrackAndConfirm.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace MeyerCorp.Usps.Core
 {
-    public interface ITrackAndConfirm
-    {
-    }
+	public interface ITrackAndConfirm
+	{
+		Task<IEnumerable<Models.TrackingInformation>> TrackAsync(params string[] trackingIds);
+
+		Task<IEnumerable<Models.TrackingInformationFields>> TrackByFieldsAsync(int revision, string clientIp, string sourceId, string destinationZipCode, DateTime? mailingDate, params string[] trackingIds);
+	}
 }
